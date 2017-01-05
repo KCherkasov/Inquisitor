34777b06af798f1e8a167dcf92ad61fd989381de
Kirill Cherkasov <kvcherk@live.ru>
2017-01-06 00:04:58 +0300

    source update

--- source/BehavioralObject.cpp
+++ source/BehavioralObject.cpp
@@ -2,22 +2,83 @@
 
 std::ifstream& operator >> (std::ifstream& stream, Approvals& data) {
   if (stream.is_open()) {
-    stream >> data._factions;
-    stream >> data._classes;
-    stream >> data._origins;
-    stream >> data._genders;
-    stream >> data._stats;
+    size_t counter = SIZE_T_DEFAULT_VALUE;
+    stream >> counter;
+    if (counter > FREE_UID) {
+      size_t key = SIZE_T_DEFAULT_VALUE;
+      ssize_t value = SSIZE_T_DEFAULT_VALUE;
+      for (size_t i = 0; i < counter; ++i) {
+        stream >> key >> value;
+        data._factions[key] = value;
+      }
+    }
+    stream >> counter;
+    if (counter > FREE_UID) {
+      size_t key = SIZE_T_DEFAULT_VALUE;
+      ssize_t value = SSIZE_T_DEFAULT_VALUE;
+      for (size_t i = 0; i < counter; ++i) {
+        stream >> key >> value;
+        data._classes[key] = value;
+      }
+    }
+    stream >> counter;
+    if (counter > FREE_UID) {
+      size_t key = SIZE_T_DEFAULT_VALUE;
+      ssize_t value = SSIZE_T_DEFAULT_VALUE;
+      for (size_t i = 0; i < counter; ++i) {
+        stream >> key >> value;
+        data._origins[key] = value;
+      }
+    }
+    stream >> counter;
+    if (counter > FREE_UID) {
+      bool key = BOOL_DEFAULT_VALUE;
+      ssize_t value = SSIZE_T_DEFAULT_VALUE;
+      for (size_t i = 0; i < counter; ++i) {
+        stream >> key >> value;
+        data._genders[key] = value;
+      }
+    }
+    stream >> counter;
+    if (counter > FREE_UID) {
+      size_t key = SIZE_T_DEFAULT_VALUE;
+      ssize_t value = SSIZE_T_DEFAULT_VALUE;
+      for (size_t i = 0; i < counter; ++i) {
+        stream >> key >> value;
+        data._stats[key] = value;
+      }
+    }
   }
   return stream;
 }
 
 std::ofstream& operator << (std::ofstream& stream, const Approvals& data) {
   if (stream.is_open()) {
-    stream << data._factions;
-    stream << data._classes;
-    stream << data._origins;
-    stream << data._genders;
-    stream << data._stats;
+    size_t counter = data._factions.size();
+    stream << counter;
+    for (std::map<size_t, ssize_t>::iterator elem = data._factions.begin(); elem != data._factions.end(); ++elem) {
+      stream << elem->first << elem->second;
+    }
+    counter = data._classes.size();
+    stream << counter;
+    for (std::map<size_t, ssize_t>::iterator elem = data._classes.begin(); elem != data._classes.end(); ++elem) {
+      stream << elem->first << elem->second;
+    }
+    counter = data._origins.size();
+    stream << counter;
+    for (std::map<size_t, ssize_t>::iterator elem = data._origins.begin(); elem != data._origins.end(); ++elem) {
+      stream << elem->first << elem->second;
+    }
+    counter = data._genders.size();
+    stream << counter;
+    for (std::map<bool, ssize_t>::iterator elem = data._genders.begin(); elem != data._genders.end(); ++elem) {
+      stream << elem->first << elem->second;
+    }
+    counter = data._stats.size();
+    stream << counter;
+    for (std::map<size_t, ssize_t>::iterator elem = data._stats.begin(); elem != data._stats.end(); ++elem) {
+      stream << elem->first << elem->second;
+    }
   }
   return stream;
 }
--- source/Misc.h
+++ source/Misc.h
@@ -8,6 +8,13 @@
 
 enum ResponseCodes { RC_OK, RC_BAD_INPUT, RC_BAD_INDEX, RC_NOT_EMPTY, RC_SIZE };
 
+const bool BOOL_DEFAULT_VALUE = false;
+const size_t SIZE_T_DEFAULT_VALUE = 0;
+const ssize_t SSIZE_T_DEFAULT_VALUE = 0;
+
+const ssize_t FREE_ID = -1;
+const size_t FREE_UID = 0;
+
 const size_t START_LEVEL = 1;
 
 #endif
